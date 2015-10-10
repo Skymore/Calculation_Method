42f7442de77eee218f7dc1537a51a373dde8c23a
Skymbp <sky@taoruideMacBook-Pro.local>
2015-10-10 14:21:25 +0800

    生成pdf，完成埃尔米特插值

--- Hermite-interpolation.py
+++ Hermite-interpolation.py
@@ -1,10 +1,124 @@
 # -*- coding: utf-8 -*-
 
-# Python Hermite-interpolation 
+#-------------------------------------------------------
+# Python Hermite-interpolation 埃尔米特插值
 #Author	: Skymore 122345615@qq.com
-#Date	: 2015-10-10
-
+#Date	: 2015-10-11
+#-------------------------------------------------------
+#H2n+1(x) = sum(j=0,n) [y[j]*Aj(x) + y'[j]*Bj(x)]
+#Li(x) = 连乘j = 0..n,j!=i  (x - X[j]) / (X[i] - X[j])
+#Ai(x) = [1 - 2(x-X[i])
+#		*sum(j=0,n, j!=i) (1/(X[i]-X[j]))]*Li^2(x)
+#Bi(x) = (x - X[i])*Li^2(x)
+#-------------------------------------------------------
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import integrate
 
+#拉格朗日插值基函数
+def L(i, t, X):
+	Li = np.float64(1)
+	for j in range(0, X.size):
+		if(j != i):
+			Li *= (t - X[j]) / (X[i] - X[j])
+	return Li
+
+#Li(t)在 t = X[i]的导数
+def Ld(i, t, X):
+	summ = np.float64(0)
+	for j in range(0, X.size):
+		if(j != i):
+			summ += 1/(X[i] - X[j])
+	return summ
+
+def A(i, t, X):
+	Ai = (1 - 2 * (t - X[i]) * Ld(i, t, X)) * (L(i, t, X)**2)
+	return Ai
+
+def B(i, t, X):
+	Bi = (t - X[i]) * (L(i, t, X)**2)
+	return Bi
+
+def H(t, X, Y, Yd):
+	ans = 0
+	for i in range(0, X.size):
+		ans += Y[i] * A(i, t, X) + Yd[i] * B(i, t, X)
+	return ans
+
+y = lambda x: 1 / (1 + 25 * x**2)
+yd = lambda x: -50*x / (625 * x**4 + 50 * x**2 + 1)
+
+if __name__ == '__main__':
+	#X Y为生成插值函数的数据，testX testY为测试插值函数图像的数据
+	#n为插值次数,X[i](i = 0...n)    X[0] = a, X[n] = b 等间距把[a,b]分为n份
+
+	a = -10
+	b = 10
+	nBest = 2
+	errBest = 9999
+	for n in range(2, 21, 2):
+		X = np.linspace(a, b, n + 1)
+		Y = y(X)
+		Yd = yd(X)
+
+		#Hn为进行插值后得到的函数。
+		Hn = lambda t: H(t, X, Y, Yd)
+
+		integrand = lambda x: abs(Hn(x) - y(x))
+		err, error = integrate.quad(integrand, a, b, limit = 101)
+		print "n = ", n, "err = ", err
+		if(err < errBest):
+			nBest = n
+			errBest = err
+	print "nBest = ", nBest
+	print "errBest = ", errBest
+
+	n = nBest
+
+	X = np.linspace(a, b, n + 1)
+	Y = y(X)
+	Yd = yd(X)
+	Hn = lambda t: H(t, X, Y, Yd)
+	testX = np.linspace(a,b,201)
+	testY = y(testX)
+	testYd = yd(testX)
+	testF = H(testX,X,Y,Yd)
+
+	Fig = plt.figure(1)
+	#subplot(221)把绘图区域等分为2行*2列共4个区域,
+	#然后在区域1(左上区域)中创建一个轴对象.
+	#subplot(222)在区域2(左上区域)中创建一个轴对象.
+	#subplot(212)把绘图区域等分为2行*1列共2个区域,
+	#pl.subplot(212)在区域2(下区域)创建一个轴对象.
+	ax1 = plt.subplot(311)
+	ax2 = plt.subplot(312)
+	ax3 = plt.subplot(313)
+	
+	#左上画f(x)及其取点和Hn(x)图像
+	plt.sca(ax1)
+	plt.title(u"埃尔米特插值 , n = %d, err = %.2f" % (nBest, errBest))
+	plt.plot(X, Y, 'ro')
+	plt.plot(testX, testY, color = "r", 
+			linestyle = "-", label = "f(x)")
+	plt.plot(testX, testF, color = "b", 
+			linestyle = "-", label = "Hn(x)")
+	plt.legend(loc='upper left')
+
+	#右上画f'(x)及其取点
+	plt.sca(ax2)
+	plt.plot(X, Yd, 'ro')
+	plt.plot(testX, testYd, color = "r", 
+			linestyle = "-", label = "f'(x)")
+	plt.plot(testX, 0*testY, color = "black", linestyle = "--")
+	plt.legend(loc='upper left')
+
+	#下方画Hn(x) - f(x)，即误差
+	plt.sca(ax3)
+	plt.plot(testX, testF - testY, color = "g", 
+			linestyle = "-", label = "Pn(x) - f(x)")
+	plt.plot(testX, 0*testY, color = "black", linestyle = "--")
+	plt.legend(loc='upper left')
+
+	#plt.show()
+	Fig.savefig("Hermite-interpolation.pdf")
--- Lagrange-interpolation.py
+++ Lagrange-interpolation.py
@@ -1,39 +1,44 @@
 # -*- coding: utf-8 -*-
 
+#-------------------------------------------------------
 # Python Lagrange-interpolation 拉格朗日n次插值
 #Author	: Skymore 122345615@qq.com
 #Date	: 2015-10-10
+#-------------------------------------------------------
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate
 
+#拉格朗日插值基函数
+def L(i, t, X):
+	Li = 1
+	for j in range(0, X.size):
+		if(j != i):
+			Li *= (t - X[j]) / (X[i] - X[j])
+	return Li
+
 #对数据（Xi, Yi）进行插值，次数为X.size
-def P(x, X, Y):
-	def L(i, x):
-		Li = 1
-		for j in range(0, X.size):
-			if(j != i):
-				Li *= (x - X[j]) / (X[i] - X[j])
-		return Li
+def P(t, X, Y):
 	ans = 0
 	for i in range(0, X.size):
-		ans += Y[i]*L(i, x)
+		ans += Y[i]*L(i, t, X)
 	return ans
 
 y = lambda x: 1 / (1 + 25 * x ** 2)
-#y = lambda x: 3 * x**3 + 2.3 * x **2 + 1.5
+#y = lambda x: 3 * x**4 + 2.3 * x **2 + 1.5
+#y = lambda x: 1/(3*x**6 + 1.3*x**2 + 1)
 
 if __name__ == '__main__':
 	#X Y为生成插值函数的数据，testX testY为测试插值函数图像的数据
 	#n为插值次数,X[i](i = 0...n)    X[0] = a, X[n] = b 等间距把[a,b]分为n份
 
-	a = -1
-	b = 1
+	a = -3
+	b = 3
 
 	nBest = 2
 	errBest = 9999
-	for n in range(2, 31, 2):
+	for n in range(2, 21, 2):
 		X = np.linspace(a, b, n + 1)
 		Y = y(X)
 
@@ -42,30 +47,44 @@ if __name__ == '__main__':
 
 		integrand = lambda x: abs(Pn(x) - y(x))
 
-		err ,error = integrate.quad(integrand, a, b, limit = 101)
+		err, error = integrate.quad(integrand, a, b, limit = 101)
 
 		print "n = ", n, "err = ", err
 		if(err < errBest):
 			nBest = n
 			errBest = err
-	print "nBest = ",nBest
+	print "nBest = ", nBest
+	print "errBest = ", errBest
 
-	n = 10
+	n = nBest
 	X = np.linspace(a, b, n + 1)
 	Y = y(X)
 	Pn = lambda x: P(x, X, Y)
-	testX = np.linspace(a,b,200001)
+	testX = np.linspace(a,b,20001)
 	testY = y(testX)
 	testF = Pn(testX)
 	#for i in range(0, testX.size):
-	#	if (abs(testY[i] - testF[i])> 0.01):
-	#		print i
-	#		print "ERROR"
+		#if (abs(testY[i] - testF[i])> 0.01):
+			#print i
+			#print "ERROR"
 
 	#up把插值所得的函数图像向上提升up个数值，方便比较
-	up = 0
-	figure = plt.figure()
+	Fig = plt.figure(1)
+
+	ax1 = plt.subplot(211)
+	ax2 = plt.subplot(212)
+	plt.sca(ax1)
+	plt.title(u"拉格朗日插值 , n = %d, err = %.2f" % (nBest, errBest))
 	plt.plot(X, Y, 'ro')
-	plt.plot(testX, testY, color = "green")
-	plt.plot(testX, testF + up, color = "blue")
-	plt.show()
+	plt.plot(testX, testY, color = "r", 
+			linestyle = "-", label = "f(x)")
+	plt.plot(testX, testF, color = "b", 
+			linestyle = "-", label = "Pn(x)")
+	plt.legend(loc='upper left')
+	plt.sca(ax2)
+	plt.plot(testX, testF - testY, color = "g", 
+			linestyle = "-", label = "Pn(x) - f(x)")
+	plt.plot(testX, 0*testY, color = "black", linestyle = "--")
+	plt.legend(loc='upper left')
+	#plt.show()
+	Fig.savefig("Hermite-interpolation.pdf")
